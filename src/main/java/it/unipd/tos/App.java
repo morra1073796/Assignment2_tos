e40059ca3e04e8dfa14c4d8808ccdaec1913948e
maria <maria.morra@studenti.unipd.it>
2020-12-08 18:10:47 +0100

    setup del progetto con maven e CI. close #1, close #2, close #3, close #4

---
+++ src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [Maria] [Morra] [1073796]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
